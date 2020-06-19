99937e728b68751f2a95f95f82ba3b460313adf3
Crowdbotics <team@crowdbotics.com>
2020-06-19 01:10:14 +0000

    Update models

--- backend/run_from_the_monste_18241/wsgi.py
+++ backend/run_from_the_monste_18241/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'run_from_the_monste_18241.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "run_from_the_monste_18241.settings")
 
 application = get_wsgi_application()
